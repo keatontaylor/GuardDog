772d66889dc653d4ba1010662669ebb407929607
Keaton Taylor <keaton@geekologist.net>
2012-08-30 14:08:38 -0500

    Added license and fixed doctoring issues.

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
-""" Default libs. """
+"""
+ Intelligent Security System - main.py - v0.1
+ Created: 05/2012
+ Author: Keaton Taylor - keaton@keatonstaylor.com - www.keatonstaylor.com
+ Website: https://github.com/keatontaylor/GuardDog
+ 
+ A continuously running python script that monitors the state of 
+ each zone in the system and updates a sqlite databse when the zone
+ changes state.
+
+ Copyright 2012 Keaton Taylor
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+"""
+
+# Defualt libs.
 from smtplib import SMTP_SSL
 from email.MIMEText import MIMEText
-""" User libs. """
+# User libs.
 from bbio import *
 import inc.config as config # Change this to a .cfg file and parse.
 import inc.framework as framework
 
-""" Does initial setup of the pins by settings them to pulldown inputs. """
 def setup():
+  """ Pre-run setup. Set up pins as Pulldown INPUTS. """
   for zone in framework.zones.getZones():
     pinMode(zone.pin, INPUT, -1)
 
-
-""" Loops continously pooling each of the pins connected to each zone.
-    Checks if the zone has changed state then updates the database. """
 def loop():
+  """ Loops continously pooling each of the pins connected to each zone.
+	   Checks if the zone has changed state then updates the database. """
   for zone in framework.zones.getZones():
     curstate = digitalRead(zone.pin)
     if curstate == 0 and zone.state == True:
@@ -28,42 +52,17 @@ def loop():
       zone.lastevent = time.time()
       zone.timeslo = 0
       updatedb(zone)
-    if curstate == 0: 
-      if (zone.lastevent + 300) < time.time():
-        print zone.name + " Left Opened"
-        zone.lastevent = time.time()
-        sendemail(zone)
   toggle(USR3) # Show activity that this script is running.
   delay(500) # Give the CPU a little break.				
 
-""" Updates the database with the new zone state and last event time. """
+
 def updatedb(zone):
+  """ Updates the database with the new zone state and last event time. """
   con = framework.lite.connect('/etc/SmartHome/Databases/Security.sqlite')
   cur = con.cursor()
   cur.execute("INSERT INTO Log(Time, Zone, State) VALUES(?, ?, ?)", [zone.lastevent, zone.name, zone.state])
   con.commit()
   con.close()
-
-""" Sends email to the recepients in the config file using the smtp credentials
-    in the config file. """
-# TODO: Move into framework.
-def sendemail(zone):
-  zone.timeslo = zone.timeslo + 1
-  timeslo = (zone.timeslo * 5)
-  msg = MIMEText(zone.name + " Left Opened For: " + str(timeslo)  + "min." , "plain")
-  msg['Subject']= ""
-  msg['From'] = "emailuser" # some SMTP servers will do this automatically, not all
-
-  try:
-    smtpserver = smtplib.SMTP("emailserver",587)
-    smtpserver.ehlo()
-    smtpserver.starttls()
-    smtpserver.ehlo
-    smtpserver.login('emailuser', 'emailpass')
-    smtpserver.sendmail('emailuser', 'to', msg.as_string())
-    smtpserver.close()
-  except: 
-    print "Message could not be sent. Likely due to the internet connection being down."
 
 run (setup, loop)
 
